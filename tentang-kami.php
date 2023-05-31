cac3fdd6738b83df223b4eb2aa3b4883ade4a28a
bayu adhi prastyo <bayuadhiprastyo@gmail.com>
2023-05-31 23:46:57 +0700

    first commit

---
+++ tentang-kami.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Online | Tentang Kami</title>
+    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free-6.4.0-web/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <?php require "navbar.php"; ?>
+    
+    <!-- banner -->
+    <div class="container-fluid banner2  d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">Tentang Kami</h1>
+        </div>
+    </div>
+            <!-- tentang kami -->
+            <div class="container-fluid py-5">
+                <div class="container fs-5 text-center">
+                    <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quis 
+                    commodi vel distinctio natus! Error, magnam unde veniam 
+                    perspiciatis voluptas eius minima non? Possimus doloribus 
+                    officia a repellat, ad maxime.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Nulla atque ad possimus explicabo aliquid eum expedita cumque 
+                        repellat sapiente laudantium ab doloremque, 
+                        ipsam sit molestias necessitatibus, iure animi quasi facilis amet 
+                        consequuntur repellendus, adipisci fugiat pariatur quod? 
+                        Id ipsum cumque distinctio repellendus facere exercitationem 
+                        natus cupiditate repellat reiciendis amet iste, nemo quaerat iure, 
+                        dolorum veniam repudiandae consequatur at. Itaque nobis enim, 
+                        quos et dolores cumque magnam dicta suscipit unde iure? Doloremque ipsum 
+                        dolor quia ab aut, dicta rerum accusamus iure.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Fuga perspiciatis, voluptas illum officiis pariatur, doloribus dolorem 
+                        accusantium non ea nisi cumque odit nemo quia nesciunt sunt nam minus
+                        adipisci dignissimos magni, aut quo quam corporis quisquam. Magni sapiente 
+                        deleniti officiis reiciendis inventore, nam facilis, accusamus quos 
+                        praesentium labore ducimus voluptatum! Rerum, beatae recusandae sunt, 
+                        amet quia, sit placeat laboriosam error non facilis dolore dignissimos
+                        officia quos aut necessitatibus sequi eaque doloribus officiis labore?
+                        Dolorem quasi error veniam laudantium sunt expedita ipsam esse nam, 
+                        recusandae earum ex accusamus libero tempora eius soluta. Recusandae, 
+                        quaerat, voluptatum hic suscipit possimus corrupti pariatur saepe aliquid 
+                        voluptates, eum voluptatibus commodi dolor nulla! Asperiores natus soluta 
+                        sed minus nulla aspernatur accusantium explicabo maxime esse eligendi, 
+                        illum odio, consequuntur molestiae cum praesentium, voluptate possimus 
+                        sequi modi veritatis aliquam aperiam reiciendis id ipsum obcaecati. 
+                        Aliquam pariatur dolor minima consectetur! Alias corrupti porro cum 
+                        itaque nisi nesciunt culpa dolore.
+                    </p>
+                </div>
+            </div>
+
+            <?php require "footer.php"; ?>
+            <script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+            <script src="fontawesome-free-6.4.0-web/js/all.min.js"></script>
+
+</body>
+</html>
